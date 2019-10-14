46e53829d9be57963685b646e963a6e0c3d37796
cutterpoint <xiaofengbly@163.com>
2019-10-14 22:21:17 +0800

    cutter_point改库

--- cutter_point_blog_picture/src/main/java/com/cutter/point/blog/picture/restapi/CkEditorRestApi.java
+++ cutter_point_blog_picture/src/main/java/com/cutter/point/blog/picture/restapi/CkEditorRestApi.java
@@ -23,11 +23,11 @@ import com.cutter.point.blog.picture.util.Aboutfile;
 @RequestMapping("/ckeditor")
 public class CkEditorRestApi {
 	
-	@Value(value="${file.upload.path}") //获取基本路径
-	private String basePath;
+//	@Value(value="${file.upload.path}") //获取基本路径
+//	private String basePath;
 	
-	@Value(value="${data.image.url}")
-	private String imgURL;
+//	@Value(value="${data.image.url}")
+//	private String imgURL;
 
     private String uploadImageUrl= "ckEditorUploadImg";//图像存放路径
     
@@ -51,75 +51,75 @@ public class CkEditorRestApi {
 		return ext;
 	}
 	
-    /** 
-     * 图像中的图片上传 
-     *  
-     * @return 
-     * @throws IOException 
-     */
-    @RequestMapping(value="/imgUpload", method=RequestMethod.POST)
-    public Object imgUpload(HttpServletRequest request, HttpServletResponse response) throws IOException {  
-    		
-    	Map<String, Object> map = new HashMap<String, Object>();
-    	Map<String, Object> errorMap = new HashMap<String, Object>();
-        Aboutfile af=new Aboutfile();//引用自己设计的一个工具类
-        
-        // 转换成多部分request
-        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-        // 取得request中的所有文件名
-        Iterator<String> iter = multiRequest.getFileNames();
-        while(iter.hasNext()) {
-        	MultipartFile file = multiRequest.getFile(iter.next());
-        	if (file != null) {
-        		                
-        		String oldName = file.getOriginalFilename(); //获取旧名称
-        		String expandedName = getPicExpandedName(oldName); //获取扩展名        		
-
-                if (!af.isPic(expandedName)){ //判断是否是图片 
-                	map.put("uploaded", 0);
-                	errorMap.put("message", "请上传正确的图片");
-                	map.put("error", errorMap);
-                    return map;  
-                }
-                
-                if (file.getSize() > (2*1024 * 1024)) {//对图片大小进行限制
-                	map.put("uploaded", 0);
-                	errorMap.put("message", "图片大小不能超过2M");
-                	map.put("error", errorMap);
-                    return map;  
-                }  
-
-                //图片上传路径  
-                String uploadPath = basePath + uploadImageUrl;  
-                String fileName = System.currentTimeMillis()+"" ; // 采用时间格式命名  
-                fileName +=("."+expandedName);
-                String fileLocation = uploadPath+"/"+fileName;
-        		//上传文件用的是个人工具类上传文件
-                
-    			File file1 = new File(uploadPath);
-				if (!file1.exists()) {
-					file1.mkdirs();
-				}
-				try {
-					File saveFile = new File(fileLocation);
-					saveFile.createNewFile();
-					file.transferTo(saveFile);				
-                	map.put("uploaded", 1);
-                	map.put("fileName", fileName);
-                	map.put("url", imgURL + uploadImageUrl+"/" + fileName);
-	                return map;
-				} catch (Exception e) {
-                	map.put("uploaded", 0);
-                	errorMap.put("message", "上传失败");
-                	map.put("error", errorMap);
-                    return map;  
-				}				
-
-        	}
-        }
-		return null;
-        
-    }  
+//    /**
+//     * 图像中的图片上传
+//     *
+//     * @return
+//     * @throws IOException
+//     */
+//    @RequestMapping(value="/imgUpload", method=RequestMethod.POST)
+//    public Object imgUpload(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//
+//    	Map<String, Object> map = new HashMap<String, Object>();
+//    	Map<String, Object> errorMap = new HashMap<String, Object>();
+//        Aboutfile af=new Aboutfile();//引用自己设计的一个工具类
+//
+//        // 转换成多部分request
+//        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+//        // 取得request中的所有文件名
+//        Iterator<String> iter = multiRequest.getFileNames();
+//        while(iter.hasNext()) {
+//        	MultipartFile file = multiRequest.getFile(iter.next());
+//        	if (file != null) {
+//
+//        		String oldName = file.getOriginalFilename(); //获取旧名称
+//        		String expandedName = getPicExpandedName(oldName); //获取扩展名
+//
+//                if (!af.isPic(expandedName)){ //判断是否是图片
+//                	map.put("uploaded", 0);
+//                	errorMap.put("message", "请上传正确的图片");
+//                	map.put("error", errorMap);
+//                    return map;
+//                }
+//
+//                if (file.getSize() > (2*1024 * 1024)) {//对图片大小进行限制
+//                	map.put("uploaded", 0);
+//                	errorMap.put("message", "图片大小不能超过2M");
+//                	map.put("error", errorMap);
+//                    return map;
+//                }
+//
+//                //图片上传路径
+//                String uploadPath = basePath + uploadImageUrl;
+//                String fileName = System.currentTimeMillis()+"" ; // 采用时间格式命名
+//                fileName +=("."+expandedName);
+//                String fileLocation = uploadPath+"/"+fileName;
+//        		//上传文件用的是个人工具类上传文件
+//
+//    			File file1 = new File(uploadPath);
+//				if (!file1.exists()) {
+//					file1.mkdirs();
+//				}
+//				try {
+//					File saveFile = new File(fileLocation);
+//					saveFile.createNewFile();
+//					file.transferTo(saveFile);
+//                	map.put("uploaded", 1);
+//                	map.put("fileName", fileName);
+//                	map.put("url", imgURL + uploadImageUrl+"/" + fileName);
+//	                return map;
+//				} catch (Exception e) {
+//                	map.put("uploaded", 0);
+//                	errorMap.put("message", "上传失败");
+//                	map.put("error", errorMap);
+//                    return map;
+//				}
+//
+//        	}
+//        }
+//		return null;
+//
+//    }
     
     /** 
      * 工具栏“插入\编辑超链接”的文件上传 
@@ -128,68 +128,68 @@ public class CkEditorRestApi {
      * @throws IOException 
      */
     
-    @RequestMapping(value="/fileUpload", method=RequestMethod.POST)
-    public Object fileUpload(HttpServletRequest request, HttpServletResponse response) throws IOException {  
-    	  
-  
- 
-    	Map<String, Object> map = new HashMap<String, Object>();
-    	Map<String, Object> errorMap = new HashMap<String, Object>();
-        Aboutfile af=new Aboutfile();//引用自己设计的一个工具类
-        
-        // 转换成多部分request
-        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-        // 取得request中的所有文件名
-        Iterator<String> iter = multiRequest.getFileNames();
-        while(iter.hasNext()) {
-        	MultipartFile file = multiRequest.getFile(iter.next());
-        	if (file != null) {
-        		                
-        		String oldName = file.getOriginalFilename(); //获取旧名称
-        		String expandedName = getPicExpandedName(oldName); //获取扩展名        		
-
-                if (!af.isSafe(expandedName)){ //判断是否安全文件
-                	map.put("uploaded", 0);
-                	errorMap.put("message", "请上传正确的图片");
-                	map.put("error", errorMap);
-                    return map;  
-                }
-                
-                if (file.getSize() > ( 50 *1024 * 1024)) {//对图片大小进行限制
-                	map.put("uploaded", 0);
-                	errorMap.put("message", "文件大小不能超过50M");
-                	map.put("error", errorMap);
-                    return map;  
-                }  
-
-                //图片上传路径  
-                String uploadPath = basePath + uploadFileUrl;  
-                String fileName = System.currentTimeMillis()+"" ; // 采用时间格式命名  
-                fileName +=("."+expandedName);
-                String fileLocation = uploadPath+"/"+fileName;
-        		//上传文件用的是个人工具类上传文件
-                
-    			File file1 = new File(uploadPath);
-				if (!file1.exists()) {
-					file1.mkdirs();
-				}
-				try {
-					File saveFile = new File(fileLocation);
-					saveFile.createNewFile();
-					file.transferTo(saveFile);				
-                	map.put("uploaded", 1);
-                	map.put("fileName", fileName);
-                	map.put("url", imgURL + uploadFileUrl+"/" + fileName);
-	                return map;
-				} catch (Exception e) {
-                	map.put("uploaded", 0);
-                	errorMap.put("message", "上传失败");
-                	map.put("error", errorMap);
-                    return map;  
-				}				
-
-        	}
-        }
-		return null;
-    } 
+//    @RequestMapping(value="/fileUpload", method=RequestMethod.POST)
+//    public Object fileUpload(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//
+//
+//
+//    	Map<String, Object> map = new HashMap<String, Object>();
+//    	Map<String, Object> errorMap = new HashMap<String, Object>();
+//        Aboutfile af=new Aboutfile();//引用自己设计的一个工具类
+//
+//        // 转换成多部分request
+//        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+//        // 取得request中的所有文件名
+//        Iterator<String> iter = multiRequest.getFileNames();
+//        while(iter.hasNext()) {
+//        	MultipartFile file = multiRequest.getFile(iter.next());
+//        	if (file != null) {
+//
+//        		String oldName = file.getOriginalFilename(); //获取旧名称
+//        		String expandedName = getPicExpandedName(oldName); //获取扩展名
+//
+//                if (!af.isSafe(expandedName)){ //判断是否安全文件
+//                	map.put("uploaded", 0);
+//                	errorMap.put("message", "请上传正确的图片");
+//                	map.put("error", errorMap);
+//                    return map;
+//                }
+//
+//                if (file.getSize() > ( 50 *1024 * 1024)) {//对图片大小进行限制
+//                	map.put("uploaded", 0);
+//                	errorMap.put("message", "文件大小不能超过50M");
+//                	map.put("error", errorMap);
+//                    return map;
+//                }
+//
+//                //图片上传路径
+//                String uploadPath = basePath + uploadFileUrl;
+//                String fileName = System.currentTimeMillis()+"" ; // 采用时间格式命名
+//                fileName +=("."+expandedName);
+//                String fileLocation = uploadPath+"/"+fileName;
+//        		//上传文件用的是个人工具类上传文件
+//
+//    			File file1 = new File(uploadPath);
+//				if (!file1.exists()) {
+//					file1.mkdirs();
+//				}
+//				try {
+//					File saveFile = new File(fileLocation);
+//					saveFile.createNewFile();
+//					file.transferTo(saveFile);
+//                	map.put("uploaded", 1);
+//                	map.put("fileName", fileName);
+//                	map.put("url", imgURL + uploadFileUrl+"/" + fileName);
+//	                return map;
+//				} catch (Exception e) {
+//                	map.put("uploaded", 0);
+//                	errorMap.put("message", "上传失败");
+//                	map.put("error", errorMap);
+//                    return map;
+//				}
+//
+//        	}
+//        }
+//		return null;
+//    }
 }
--- cutter_point_blog_picture/src/main/java/com/cutter/point/blog/picture/restapi/FileRestApi.java
+++ cutter_point_blog_picture/src/main/java/com/cutter/point/blog/picture/restapi/FileRestApi.java
@@ -60,11 +60,11 @@ public class FileRestApi {
 	@Autowired
 	private FileSortService fileSortService;
 	
-	@Value(value="${data.image.url}")
-	private String imgHost;
+//	@Value(value="${data.image.url}")
+//	private String imgHost;
 	
-	@Value(value="${file.upload.path}") //获取上传路径
-	private String path;
+//	@Value(value="${file.upload.path}") //获取上传路径
+//	private String path;
 
 	@Value(value = "${file.size}") //单个文件的大小
 	private int size;
@@ -115,38 +115,38 @@ public class FileRestApi {
 	 *  code ids用什么分割的，默认“,”
 	 * @return
 	 */
-	@ApiOperation(value="通过fileIds获取图片信息接口", notes="获取图片信息接口")
-	@GetMapping("/getPicture")
-	public String getPicture(
-			@ApiParam(name = "fileIds", value = "文件ids",required = false) @RequestParam(name = "fileIds", required = false) String fileIds,
-			@ApiParam(name = "code", value = "切割符",required = false) @RequestParam(name = "code", required = false) String code) {
-
-		if(StringUtils.isEmpty(code)) {
-			code = ",";
-		}
-		if(StringUtils.isEmpty(fileIds)) {
-			return ResultUtil.result(SysConf.ERROR,"数据错误");
-		}else {
-			List<Map<String, Object>> list = new ArrayList<>();
-			List<String> changeStringToString = StringUtils.changeStringToString(fileIds, code);
-			QueryWrapper<com.cutter.point.blog.picture.entity.File> queryWrapper = new QueryWrapper<>();
-			queryWrapper.in(SQLConf.UID, changeStringToString);
-			List<com.cutter.point.blog.picture.entity.File> fileList = fileService.list(queryWrapper);
-			if(fileList.size()>0) {
-				for(com.cutter.point.blog.picture.entity.File file : fileList) {
-					if(file!=null) {
-						Map<String, Object> remap = new HashMap<>();
-						remap.put("url", imgHost + file.getPicUrl());//，地址
-						remap.put("expandedName", file.getPicExpandedName());//后缀名，也就是类型
-						remap.put("name", file.getPicName());//名称
-						remap.put("uid", file.getUid());
-						list.add(remap);
-					}	
-				}					
-			}
-			return ResultUtil.result(SysConf.SUCCESS,list);
-		}
-	}
+//	@ApiOperation(value="通过fileIds获取图片信息接口", notes="获取图片信息接口")
+//	@GetMapping("/getPicture")
+//	public String getPicture(
+//			@ApiParam(name = "fileIds", value = "文件ids",required = false) @RequestParam(name = "fileIds", required = false) String fileIds,
+//			@ApiParam(name = "code", value = "切割符",required = false) @RequestParam(name = "code", required = false) String code) {
+//
+//		if(StringUtils.isEmpty(code)) {
+//			code = ",";
+//		}
+//		if(StringUtils.isEmpty(fileIds)) {
+//			return ResultUtil.result(SysConf.ERROR,"数据错误");
+//		}else {
+//			List<Map<String, Object>> list = new ArrayList<>();
+//			List<String> changeStringToString = StringUtils.changeStringToString(fileIds, code);
+//			QueryWrapper<com.cutter.point.blog.picture.entity.File> queryWrapper = new QueryWrapper<>();
+//			queryWrapper.in(SQLConf.UID, changeStringToString);
+//			List<com.cutter.point.blog.picture.entity.File> fileList = fileService.list(queryWrapper);
+//			if(fileList.size()>0) {
+//				for(com.cutter.point.blog.picture.entity.File file : fileList) {
+//					if(file!=null) {
+//						Map<String, Object> remap = new HashMap<>();
+//						remap.put("url", imgHost + file.getPicUrl());//，地址
+//						remap.put("expandedName", file.getPicExpandedName());//后缀名，也就是类型
+//						remap.put("name", file.getPicName());//名称
+//						remap.put("uid", file.getUid());
+//						list.add(remap);
+//					}
+//				}
+//			}
+//			return ResultUtil.result(SysConf.SUCCESS,list);
+//		}
+//	}
 	
 
 	/**
@@ -156,67 +156,67 @@ public class FileRestApi {
 	 * @param request
 	 * @param response
 	 */
-	@ApiOperation(value="图片下载接口", notes="图片下载接口")
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "fileIds", value = "fileIds", required = false, dataType = "String")
-	})
-	@GetMapping("downloadFile")
-	public Object downloadFile( HttpServletRequest request ,HttpServletResponse response) {
-		String fileId = request.getParameter("fileId");
-		
-		if(StringUtils.isEmpty(fileId)) {
-			
-			com.cutter.point.blog.picture.entity.File oneById = fileService.getById(fileId);
-			if(oneById!=null) {
-				String fileName = request.getParameter("fileName");//aim_test//文件名,不传就用默认的，或者是oldName
-				if(StringUtils.isEmpty(fileName)) {
-					fileName = oneById.getFileOldName();//以前的名字
-				}
-				
-				String fileRealPath = path + oneById.getPicUrl();
-				File file = new File(fileRealPath);
-				response.setContentType("application/force-download"); //设置强制下载不打开
-				response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
-					
-				byte[] buffer = new byte[1024];
-				FileInputStream fis = null;
-				BufferedInputStream bis = null;
-				try {
-					fis = new FileInputStream(file);
-					bis = new BufferedInputStream(fis);
-					OutputStream os = response.getOutputStream();
-					int i = bis.read(buffer);
-					while (i != -1) {
-						os.write(buffer, 0, i);
-						i = bis.read(buffer);
-					}
-					log.info("文件下载成功 realUrl:   "+fileRealPath);
-					return ResultUtil.result(SysConf.SUCCESS, null);
-				} catch (Exception e) {
-					e.printStackTrace();
-					log.info("============文件下载出现异常==========");
-					return ResultUtil.result(SysConf.ERROR, "文件下载出现异常");
-				} finally {
-					if (bis != null) {
-						try {
-							bis.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-					if (fis != null) {
-						try {
-							fis.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		}
-		log.info("文件下载失败fileId=   "+fileId);
-		return ResultUtil.result(SysConf.ERROR, "参数错误");
-	}
+//	@ApiOperation(value="图片下载接口", notes="图片下载接口")
+//	@ApiImplicitParams({
+//			@ApiImplicitParam(name = "fileIds", value = "fileIds", required = false, dataType = "String")
+//	})
+//	@GetMapping("downloadFile")
+//	public Object downloadFile( HttpServletRequest request ,HttpServletResponse response) {
+//		String fileId = request.getParameter("fileId");
+//
+//		if(StringUtils.isEmpty(fileId)) {
+//
+//			com.cutter.point.blog.picture.entity.File oneById = fileService.getById(fileId);
+//			if(oneById!=null) {
+//				String fileName = request.getParameter("fileName");//aim_test//文件名,不传就用默认的，或者是oldName
+//				if(StringUtils.isEmpty(fileName)) {
+//					fileName = oneById.getFileOldName();//以前的名字
+//				}
+//
+//				String fileRealPath = path + oneById.getPicUrl();
+//				File file = new File(fileRealPath);
+//				response.setContentType("application/force-download"); //设置强制下载不打开
+//				response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+//
+//				byte[] buffer = new byte[1024];
+//				FileInputStream fis = null;
+//				BufferedInputStream bis = null;
+//				try {
+//					fis = new FileInputStream(file);
+//					bis = new BufferedInputStream(fis);
+//					OutputStream os = response.getOutputStream();
+//					int i = bis.read(buffer);
+//					while (i != -1) {
+//						os.write(buffer, 0, i);
+//						i = bis.read(buffer);
+//					}
+//					log.info("文件下载成功 realUrl:   "+fileRealPath);
+//					return ResultUtil.result(SysConf.SUCCESS, null);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//					log.info("============文件下载出现异常==========");
+//					return ResultUtil.result(SysConf.ERROR, "文件下载出现异常");
+//				} finally {
+//					if (bis != null) {
+//						try {
+//							bis.close();
+//						} catch (IOException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//					if (fis != null) {
+//						try {
+//							fis.close();
+//						} catch (IOException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//				}
+//			}
+//		}
+//		log.info("文件下载失败fileId=   "+fileId);
+//		return ResultUtil.result(SysConf.ERROR, "参数错误");
+//	}
 
 
 	/**
@@ -228,118 +228,118 @@ public class FileRestApi {
 	 * @param request
 	 * @return 
 	 */
-	@ApiOperation(value="多图片上传接口", notes="多图片上传接口")
-	@ApiImplicitParams({
-			@ApiImplicitParam(name = "filedatas", value = "文件数据", required = true),
-			@ApiImplicitParam(name = "userUid", value = "用户UID", required = false, dataType = "String"),
-			@ApiImplicitParam(name = "sysUserId", value = "管理员UID", required = false, dataType = "String"),
-			@ApiImplicitParam(name = "projectName", value = "项目名", required = false, dataType = "String"),
-			@ApiImplicitParam(name = "sortName", value = "模块名", required = false, dataType = "String")
-	})
-	@PostMapping("/pictures")
-	public synchronized Object uploadPics(HttpServletResponse response,HttpServletRequest request, List<MultipartFile> filedatas) {
-		//上传者id    
-		String userUid = request.getParameter("userUid"); //如果是用户上传，则包含用户uid
-		String adminUid = request.getParameter("adminUid"); //如果是管理员上传，则包含管理员uid
-		String projectName = request.getParameter("projectName");//项目名   
-		String sortName = request.getParameter("sortName");//模块名
-		
-		//projectName现在默认base
-		if(StringUtils.isEmpty(projectName)) {
-			projectName = "base";
-		}
-		
-		//这里可以检测用户上传，如果不是网站的用户或会员就不能调用
-		if(StringUtils.isEmpty(userUid) && StringUtils.isEmpty(adminUid)) {
-			return ResultUtil.result(SysConf.ERROR, "请先注册");
-		}else{
-
-		}
-		
-		log.info("####### fileSorts" + projectName + " ###### " + sortName);
-		
-		QueryWrapper<FileSort> queryWrapper = new QueryWrapper<>();
-		queryWrapper.eq(SQLConf.SORT_NAME, sortName);
-		queryWrapper.eq(SQLConf.PROJECT_NAME, projectName);
-		queryWrapper.eq(SQLConf.STATUS, EStatus.ENABLE);
-		List<FileSort> fileSorts = fileSortService.list(queryWrapper);	
-		
-		System.out.println("fileSorts"+JsonUtils.objectToJson(fileSorts));
-		
-		FileSort fileSort = null;
-		if(fileSorts.size() > 0) {
-			fileSort = fileSorts.get(0);
-			log.info("====fileSort===="+JsonUtils.objectToJson(fileSort));
-		} else {
-			return ResultUtil.result(SysConf.ERROR, "文件不被允许上传");
-		}		
-		
-		String sortUrl = fileSort.getUrl();
-		
-		//判断url是否为空，如果为空，使用默认
-		if(StringUtils.isEmpty(sortUrl)) {
-			sortUrl = "base/common/";
-		}else {
-			sortUrl =fileSort.getUrl(); 
-		}
-		
-		List<com.cutter.point.blog.picture.entity.File> lists = new ArrayList<>();
-		//文件上传
-		if(filedatas!=null&&filedatas.size()>0) {
-			for(MultipartFile filedata:filedatas) {
-				
-				String oldName = filedata.getOriginalFilename();
-				long size = filedata.getSize();
-				//以前的文件名
-				log.info("上传文件====：" + oldName);
-				//文件大小
-				log.info("文件大小====：" + size);
-				//获取扩展名，默认是jpg
-				String picExpandedName = getPicExpandedName(oldName);
-				//获取新文件名
-				String newFileName = String.valueOf(System.currentTimeMillis() + "." + picExpandedName);
-				log.info(newFileName+":"+oldName);
-				//文件路径问题
-				log.info("path====" + path);
-				String newPath = path + sortUrl + "/" + picExpandedName + "/" + DateUtils.getYears() + "/"
-						+ DateUtils.getMonth() + "/" + DateUtils.getDay() + "/";
-				//path = path.replaceAll("//", "/");
-				String picurl = sortUrl + "/" + picExpandedName + "/" + DateUtils.getYears() + "/"
-						+ DateUtils.getMonth() + "/" + DateUtils.getDay() + "/" + newFileName;
-				log.info("newPath====" + newPath);
-				String saveUrl = newPath + newFileName;
-				File file1 = new File(newPath);
-				if (!file1.exists()) {
-					file1.mkdirs();
-				}
-				try {
-					File saveFile = new File(saveUrl);
-					saveFile.createNewFile();
-					filedata.transferTo(saveFile);
-				} catch (Exception e) {
-					log.info("==上传文件异常===url:" + saveUrl + "-----");
-					e.printStackTrace();
-					return ResultUtil.result(SysConf.ERROR, "文件上传失败");
-				}
-				com.cutter.point.blog.picture.entity.File file = new com.cutter.point.blog.picture.entity.File();
-				file.setCreateTime(new Date(System.currentTimeMillis()));
-				file.setFileSortUid(fileSort.getUid());				
-				file.setFileOldName(oldName);
-				file.setFileSize(size);
-				file.setPicExpandedName(picExpandedName);
-				file.setPicName(newFileName);
-				file.setPicUrl(picurl);
-				file.setStatus(EStatus.ENABLE);
-				file.setUserUid(userUid);
-				file.setAdminUid(adminUid);
-				fileService.save(file);
-				lists.add(file);		    	 
-			}
-			 //保存成功返回数据
-			return ResultUtil.result(SysConf.SUCCESS, lists);
-		}
-		return ResultUtil.result(SysConf.ERROR, "请上传图片");
-	}
+//	@ApiOperation(value="多图片上传接口", notes="多图片上传接口")
+//	@ApiImplicitParams({
+//			@ApiImplicitParam(name = "filedatas", value = "文件数据", required = true),
+//			@ApiImplicitParam(name = "userUid", value = "用户UID", required = false, dataType = "String"),
+//			@ApiImplicitParam(name = "sysUserId", value = "管理员UID", required = false, dataType = "String"),
+//			@ApiImplicitParam(name = "projectName", value = "项目名", required = false, dataType = "String"),
+//			@ApiImplicitParam(name = "sortName", value = "模块名", required = false, dataType = "String")
+//	})
+//	@PostMapping("/pictures")
+//	public synchronized Object uploadPics(HttpServletResponse response,HttpServletRequest request, List<MultipartFile> filedatas) {
+//		//上传者id
+//		String userUid = request.getParameter("userUid"); //如果是用户上传，则包含用户uid
+//		String adminUid = request.getParameter("adminUid"); //如果是管理员上传，则包含管理员uid
+//		String projectName = request.getParameter("projectName");//项目名
+//		String sortName = request.getParameter("sortName");//模块名
+//
+//		//projectName现在默认base
+//		if(StringUtils.isEmpty(projectName)) {
+//			projectName = "base";
+//		}
+//
+//		//这里可以检测用户上传，如果不是网站的用户或会员就不能调用
+//		if(StringUtils.isEmpty(userUid) && StringUtils.isEmpty(adminUid)) {
+//			return ResultUtil.result(SysConf.ERROR, "请先注册");
+//		}else{
+//
+//		}
+//
+//		log.info("####### fileSorts" + projectName + " ###### " + sortName);
+//
+//		QueryWrapper<FileSort> queryWrapper = new QueryWrapper<>();
+//		queryWrapper.eq(SQLConf.SORT_NAME, sortName);
+//		queryWrapper.eq(SQLConf.PROJECT_NAME, projectName);
+//		queryWrapper.eq(SQLConf.STATUS, EStatus.ENABLE);
+//		List<FileSort> fileSorts = fileSortService.list(queryWrapper);
+//
+//		System.out.println("fileSorts"+JsonUtils.objectToJson(fileSorts));
+//
+//		FileSort fileSort = null;
+//		if(fileSorts.size() > 0) {
+//			fileSort = fileSorts.get(0);
+//			log.info("====fileSort===="+JsonUtils.objectToJson(fileSort));
+//		} else {
+//			return ResultUtil.result(SysConf.ERROR, "文件不被允许上传");
+//		}
+//
+//		String sortUrl = fileSort.getUrl();
+//
+//		//判断url是否为空，如果为空，使用默认
+//		if(StringUtils.isEmpty(sortUrl)) {
+//			sortUrl = "base/common/";
+//		}else {
+//			sortUrl =fileSort.getUrl();
+//		}
+//
+//		List<com.cutter.point.blog.picture.entity.File> lists = new ArrayList<>();
+//		//文件上传
+//		if(filedatas!=null&&filedatas.size()>0) {
+//			for(MultipartFile filedata:filedatas) {
+//
+//				String oldName = filedata.getOriginalFilename();
+//				long size = filedata.getSize();
+//				//以前的文件名
+//				log.info("上传文件====：" + oldName);
+//				//文件大小
+//				log.info("文件大小====：" + size);
+//				//获取扩展名，默认是jpg
+//				String picExpandedName = getPicExpandedName(oldName);
+//				//获取新文件名
+//				String newFileName = String.valueOf(System.currentTimeMillis() + "." + picExpandedName);
+//				log.info(newFileName+":"+oldName);
+//				//文件路径问题
+//				log.info("path====" + path);
+//				String newPath = path + sortUrl + "/" + picExpandedName + "/" + DateUtils.getYears() + "/"
+//						+ DateUtils.getMonth() + "/" + DateUtils.getDay() + "/";
+//				//path = path.replaceAll("//", "/");
+//				String picurl = sortUrl + "/" + picExpandedName + "/" + DateUtils.getYears() + "/"
+//						+ DateUtils.getMonth() + "/" + DateUtils.getDay() + "/" + newFileName;
+//				log.info("newPath====" + newPath);
+//				String saveUrl = newPath + newFileName;
+//				File file1 = new File(newPath);
+//				if (!file1.exists()) {
+//					file1.mkdirs();
+//				}
+//				try {
+//					File saveFile = new File(saveUrl);
+//					saveFile.createNewFile();
+//					filedata.transferTo(saveFile);
+//				} catch (Exception e) {
+//					log.info("==上传文件异常===url:" + saveUrl + "-----");
+//					e.printStackTrace();
+//					return ResultUtil.result(SysConf.ERROR, "文件上传失败");
+//				}
+//				com.cutter.point.blog.picture.entity.File file = new com.cutter.point.blog.picture.entity.File();
+//				file.setCreateTime(new Date(System.currentTimeMillis()));
+//				file.setFileSortUid(fileSort.getUid());
+//				file.setFileOldName(oldName);
+//				file.setFileSize(size);
+//				file.setPicExpandedName(picExpandedName);
+//				file.setPicName(newFileName);
+//				file.setPicUrl(picurl);
+//				file.setStatus(EStatus.ENABLE);
+//				file.setUserUid(userUid);
+//				file.setAdminUid(adminUid);
+//				fileService.save(file);
+//				lists.add(file);
+//			}
+//			 //保存成功返回数据
+//			return ResultUtil.result(SysConf.SUCCESS, lists);
+//		}
+//		return ResultUtil.result(SysConf.ERROR, "请上传图片");
+//	}
 
 }
 
